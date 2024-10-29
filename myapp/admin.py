ea4f1b556c25f1c2617a2c6d003358ad8dd87916
syed jibran <jibran.linkedmatrix@gmail.com>
2024-10-29 18:14:53 +0500

    Update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,19 +1,135 @@
 from django.contrib import admin
-from .models import Department, Teacher, Student, Course, Enrollment, Classroom
-from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from .models import Department, Teacher, Student, Course, Enrollment, Classroom, CustomUser
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('phone_number', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_display = ('phone_number', 'first_name', 'last_name', 'is_staff', 'is_active', 'delete_button')
     search_fields = ('phone_number', 'first_name', 'last_name')
     ordering = ('phone_number',)
 
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_customuser_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete User</a>', url)
+    delete_button.short_description = "Delete"
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'head', 'edit_button', 'delete_button')
+    search_fields = ('name',)
+    actions = ['make_uppercase']
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_department_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Department</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_department_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Department</a>', url)
+
+    def make_uppercase(self, request, queryset):
+        for department in queryset:
+            department.name = department.name.upper()
+            department.save()
+        self.message_user(request, "Department names updated to uppercase.")
+
+    make_uppercase.short_description = "Make selected departments uppercase"
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'department', 'edit_button', 'delete_button')
+    search_fields = ('first_name', 'last_name', 'email')
+    actions = ['make_uppercase']
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_teacher_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Teacher</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_teacher_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Teacher</a>', url)
+
+    def make_uppercase(self, request, queryset):
+        for teacher in queryset:
+            teacher.first_name = teacher.first_name.upper()
+            teacher.last_name = teacher.last_name.upper()
+            teacher.save()
+        self.message_user(request, "Teacher names updated to uppercase.")
+
+    make_uppercase.short_description = "Make selected teachers' names uppercase"
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'enrollment_date', 'edit_button', 'delete_button')
+    search_fields = ('first_name', 'last_name', 'email')
+    actions = ['make_uppercase']
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_student_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Student</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_student_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Student</a>', url)
+
+    def make_uppercase(self, request, queryset):
+        for student in queryset:
+            student.first_name = student.first_name.upper()
+            student.last_name = student.last_name.upper()
+            student.save()
+        self.message_user(request, "Student names updated to uppercase.")
+
+    make_uppercase.short_description = "Make selected students' names uppercase"
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'teacher', 'edit_button', 'delete_button')
+    search_fields = ('name', 'code')
+    actions = ['make_uppercase']
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_course_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Course</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_course_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Course</a>', url)
+
+    def make_uppercase(self, request, queryset):
+        for course in queryset:
+            course.name = course.name.upper()
+            course.save()
+        self.message_user(request, "Course names updated to uppercase.")
+
+    make_uppercase.short_description = "Make selected courses uppercase"
+
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'enrollment_date', 'edit_button', 'delete_button')
+    search_fields = ('student__first_name', 'course__name')
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_enrollment_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Enrollment</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_enrollment_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Enrollment</a>', url)
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('room_number', 'capacity', 'department', 'edit_button', 'delete_button')
+    search_fields = ('room_number',)
+
+    def edit_button(self, obj):
+        url = reverse('admin:myapp_classroom_change', args=[obj.id])
+        return format_html('<a class="button" href="{}">Edit Classroom</a>', url)
+
+    def delete_button(self, obj):
+        url = reverse('admin:myapp_classroom_delete', args=[obj.id])
+        return format_html('<a class="button" href="{}" style="color:red;">Delete Classroom</a>', url)
+
+# Register the models with the custom admin classes
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Department)
-admin.site.register(Teacher)
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Enrollment)
-admin.site.register(Classroom)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Enrollment, EnrollmentAdmin)
+admin.site.register(Classroom, ClassroomAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,25 @@
 from datetime import timezone
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.db import models
-from django.core.exceptions import ValidationError
 import re
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MinValueValidator
+from django.db import models
+class PhoneNumberField(models.CharField):
+    """Custom field for validating phone numbers in the format +92-XXXXXXXXX-1."""
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 15  # Set a max length constraint
+        super().__init__(*args, **kwargs)
+
+    def validate(self, value, model_instance):
+        """Custom validation to ensure the phone number follows the required format."""
+        super().validate(value, model_instance)
+        pattern = r'^\+92-\d{9}-\d{1}$'
+        if not re.match(pattern, value):
+            raise ValidationError(
+                "Phone number must be in the format '+92-XXXXXXXXX-1'."
+            )
+
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
@@ -26,18 +43,20 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(phone_number, password, **extra_fields)
 
-
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True)
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[PhoneNumberField]
+    )
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager() 
+    objects = CustomUserManager()
+
     def clean(self):
-        if not re.match(r"^\+\d{1,3}-\d{1,3}-\d{1,4}-\d{1,4}-\d{1,4}$", self.phone_number):
-            raise ValidationError("Invalid phone number format.")
-        super().clean()
+        super().clean()  
 
     def __str__(self):
         return self.phone_number
@@ -45,34 +64,71 @@ class CustomUser(AbstractUser):
 
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
-    head = models.ForeignKey('Teacher', on_delete=models.SET_NULL, null=True, related_name='departments')
-    
+    name = models.CharField(
+        max_length=100,
+        validators=[MaxLengthValidator(100, message= 'Length is greatter than 100 letters')],  # Enforces max length explicitly
+        blank=False
+    )
+    head = models.ForeignKey(
+        'Teacher',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='departments',
+        blank=True  # Allows leaving this field blank
+    )
+
     def __str__(self):
         return self.name
 
 
 class Teacher(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name='teachers')
+    first_name = models.CharField(
+        max_length=50,
+        validators=[MaxLengthValidator(50)]
+    )
+    last_name = models.CharField(
+        max_length=50,
+        validators=[MaxLengthValidator(50)]
+    )
+    email = models.EmailField(
+        unique=True,
+        validators=[MaxLengthValidator(254)],  # Standard max length for emails
+        blank=False
+    )
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='teachers',
+        blank=True
+    )
 
     def clean(self):
+        """Custom validation for email format."""
         if not re.match(r"[^@]+@[^@]+\.[^@]+", self.email):
             raise ValidationError("Invalid email format.")
-    
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
+    first_name = models.CharField(
+        max_length=50,
+        validators=[MaxLengthValidator(50)]
+    )
+    last_name = models.CharField(
+        max_length=50,
+        validators=[MaxLengthValidator(50)]
+    )
+    email = models.EmailField(
+        unique=True,
+        validators=[MaxLengthValidator(254)]
+    )
     enrollment_date = models.DateField()
 
     def clean(self):
+        """Ensure enrollment date is not in the future."""
         if self.enrollment_date > timezone.now().date():
             raise ValidationError("Enrollment date cannot be in the future.")
 
@@ -81,40 +137,80 @@ class Student(models.Model):
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=10, unique=True)
-    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, related_name='courses')
+    name = models.CharField(
+        max_length=100,
+        validators=[MaxLengthValidator(100)]
+    )
+    code = models.CharField(
+        max_length=10,
+        unique=True,
+        validators=[MaxLengthValidator(10)]
+    )
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='courses',
+        blank=True
+    )
 
     def clean(self):
+        """Custom validation for course code format."""
         if not re.match(r"^[A-Z]{3}\d{3}$", self.code):
-            raise ValidationError("Course code must be in the format 'XXX123' where X is an uppercase letter.")
+            raise ValidationError("Course code must be in the format 'XXX123'.")
 
     def __str__(self):
         return self.name
 
+
+class Classroom(models.Model):
+    room_number = models.CharField(
+        max_length=10,
+        unique=True,
+        validators=[MaxLengthValidator(10)]
+    )
+    capacity = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]  # Capacity must be at least 1
+    )
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='classrooms',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.room_number
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name='enrollments'
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='enrollments'
+    )
+    enrollment_date = models.DateField()
+
+    class Meta:
+        unique_together = ('student', 'course')
+        ordering = ['-enrollment_date', 'student']
+        verbose_name = "Enrollment"
+        verbose_name_plural = "Enrollments"
+
     def clean(self):
+        """Ensure enrollment date is not in the future."""
         if self.enrollment_date > timezone.now().date():
             raise ValidationError("Enrollment date cannot be in the future.")
 
     def __str__(self):
         return f"{self.student} enrolled in {self.course}"
 
-
-class Classroom(models.Model):
-    room_number = models.CharField(max_length=10, unique=True)
-    capacity = models.PositiveIntegerField()
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, related_name='classrooms')
-
     def clean(self):
-        if self.capacity <= 0:
-            raise ValidationError("Classroom capacity must be greater than zero.")
-
-    def __str__(self):
-        return self.room_number
-class Enrollment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='enrollments')
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrollments')
-    enrollment_date = models.DateField()
-
-    class Meta:
-        unique_together = ('student', 'course')
+        if self.enrollment_date > timezone.now().date():
+            raise ValidationError("Enrollment date cannot be in the future.")
